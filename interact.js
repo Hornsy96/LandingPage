dc3493bcf75b4977f36486289fb498114349bf06
Hornsy96 <jakehorsnell96@gmail.com>
2023-05-15 00:18:08 +1000

    added a wobble animation to the .horns element using interact.js and incorporating it while hovering over the image

--- interact.js
+++ interact.js
@@ -13,3 +13,12 @@ bouncelinks.forEach(bounce => {
     })
     });
 })
+
+const horns = document.querySelector('.horns');
+
+horns.addEventListener('mouseover', () => {
+    horn.classList.add('wobble');
+});
+horns.addEventListener('mouseout', () => {
+    horns.classList.remove('wobble');
+});
