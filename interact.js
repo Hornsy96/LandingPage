2638a44d8750b8db37a907255e0565970692e178
Hornsy96 <jakehorsnell96@gmail.com>
2023-05-15 17:23:40 +1000

    fixed a js issue where variable was incorrectly defined as bouncelinks

--- interact.js
+++ interact.js
@@ -1,12 +1,12 @@
 const links = document.querySelectorAll('.bouncelinks');
 
-bouncelinks.forEach(bounce => {
+links.forEach(bounce => {
     bounce.addEventListener('mouseover', () => {
         bounce.style.transform = 'translateY(-5px)';
         bounce.style.transition = 'transform 0.2s ease-out';
     });
 
-bouncelinks.forEach(bounce => {
+links.forEach(bounce => {
     bounce.addEventListener('mouseout', () => {
         bounce.style.transform('translateY(0)');
         bounce.style.transition('transform 0.2s ease-out')
@@ -17,7 +17,7 @@ bouncelinks.forEach(bounce => {
 const horns = document.querySelector('.horns');
 
 horns.addEventListener('mouseover', () => {
-    horn.classList.add('wobble');
+    horns.classList.add('wobble');
 });
 horns.addEventListener('mouseout', () => {
     horns.classList.remove('wobble');
