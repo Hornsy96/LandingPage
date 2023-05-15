d1dbbad2207a60ab66d2965ccacf5ad76a9377df
Hornsy96 <jakehorsnell96@gmail.com>
2023-05-15 16:58:41 +1000

    realised i split wrong js code, putback to how it was

--- contactjs.js
+++
@@ -1,8 +0,0 @@
-const horns = document.querySelector('.horns');
-
-horns.addEventListener('mouseover', () => {
-    horn.classList.add('wobble');
-});
-horns.addEventListener('mouseout', () => {
-    horns.classList.remove('wobble');
-});
--- interact.js
+++ interact.js
@@ -14,3 +14,11 @@ bouncelinks.forEach(bounce => {
     });
 })
 
+const horns = document.querySelector('.horns');
+
+horns.addEventListener('mouseover', () => {
+    horn.classList.add('wobble');
+});
+horns.addEventListener('mouseout', () => {
+    horns.classList.remove('wobble');
+});
