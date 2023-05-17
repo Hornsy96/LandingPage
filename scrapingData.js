26716272f91df373d05b98bf0386b7a6184ed39d
Jake <131365825+Hornsy96@users.noreply.github.com>
2023-05-17 12:45:34 +1000

    updated scrapingData so the data is seperated by website added html elements to retrieve data and display

--- scrapingData.js
+++ scrapingData.js
@@ -4,9 +4,10 @@ async function scrapeData() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
+  // Website 1
   await page.goto('https://www.mountainwatch.com/australia/mt-hotham/weather/');
 
-  const snowForecastData = await page.evaluate(() => {
+  const snowForecastData1 = await page.evaluate(() => {
     const forecastGraph = document.querySelector('#forecast-graph-87');
 
     // Extract temperatures
@@ -29,19 +30,92 @@ async function scrapeData() {
     return { temperatures, snowForecasts };
   });
 
-  console.log(snowForecastData); // Output the scraped data
+  // Website 2
+  await page.goto('https://www.example.com/website2');
+
+  const snowForecastData2 = await page.evaluate(() => {
+    // Scrape and process data for Website 2
+    // ...
+
+    return { temperatures, snowForecasts };
+  });
+
+  // Website 3
+  await page.goto('https://www.example.com/website3');
+
+  const snowForecastData3 = await page.evaluate(() => {
+    // Scrape and process data for Website 3
+    // ...
+
+    return { temperatures, snowForecasts };
+  });
 
   await browser.close();
+
+  // Display data for Website 1
+  displayData('website1', snowForecastData1);
+
+  // Display data for Website 2
+  displayData('website2', snowForecastData2);
+
+  // Display data for Website 3
+  displayData('website3', snowForecastData3);
 }
 
-// Function to check if the current time matches the scheduled time
+function displayData(websiteId, data) {
+  const temperatureDataElement = document.getElementById(`temperatureData${websiteId}`);
+  const snowForecastDataElement = document.getElementById(`snowForecastData${websiteId}`);
+
+  // Display temperature data
+  const temperatureTable = createTable(data.temperatures, ['Min Temp', 'Max Temp']);
+  temperatureDataElement.innerHTML = '';
+  temperatureDataElement.appendChild(temperatureTable);
+
+  // Display snow forecast data
+  const snowForecastTable = createTable(data.snowForecasts, ['Date', 'Snowfall', 'Conditions']);
+  snowForecastDataElement.innerHTML = '';
+  snowForecastDataElement.appendChild(snowForecastTable);
+}
+
+function createTable(data, headers) {
+  const table = document.createElement('table');
+  const thead = document.createElement('thead');
+  const tbody = document.createElement('tbody');
+
+  // Create table headers
+  const headerRow = document.createElement('tr');
+  headers.forEach(headerText => {
+    const th = document.createElement('th');
+    th.textContent = headerText;
+    headerRow.appendChild(th);
+  });
+  thead.appendChild(headerRow);
+
+  // Create table rows
+  data.forEach(rowData => {
+    const row = document.createElement('tr');
+    Object.values(rowData).forEach(cellData => {
+      const cell = document.createElement('td');
+      cell.textContent = cellData;
+      row.appendChild(cell);
+    });
+    tbody.appendChild(row);
+  });
+
+  table.appendChild(thead);
+  table.appendChild(tbody);
+
+  return table;
+}
+
+// Check if it's the scheduled time (e.g., 6 AM)
 function isScheduledTime() {
   const now = new Date();
   const scheduledHour = 6; // Set the desired hour for scraping (e.g., 6 AM)
   return now.getHours() === scheduledHour;
 }
 
-// Function to run the scraping logic at the scheduled time
+// Run the scraping logic if it's the scheduled time
 function runScrapingLogic() {
   if (isScheduledTime()) {
     scrapeData();
@@ -50,3 +124,4 @@ function runScrapingLogic() {
 
 // Run the code every minute
 setInterval(runScrapingLogic, 60 * 1000); // 60 seconds * 1000 milliseconds
+
