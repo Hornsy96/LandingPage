08b9036035b3ba674f0a5b4f431218354648f720
Hornsy96 <jakehorsnell96@gmail.com>
2023-06-04 16:04:02 +1000

    added images and made them buttons for RPS game used prettier to format documents

--- logicrps.js
+++ logicrps.js
@@ -1,75 +1,73 @@
-const rock = document.querySelector('#rock');
-const paper = document.querySelector('#paper');
-const scissors = document.querySelector('#scissors');
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const display = document.querySelector("#resultscreen");
 
 /* Logic to randomise the computers choice */
 function getComputerChoice() {
-    const random = Math.floor(Math.random() * 3) + 1;
-      if (random === 1) {
-        return 'rock';
-    } else if (random === 2) {
-        return 'paper';
-    } else {
-        return 'scissors'
-    }    
-};
+  const random = Math.floor(Math.random() * 3) + 1;
+  if (random === 1) {
+    return "rock";
+  } else if (random === 2) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
 
-let playerSelection = '';
-let computerSelection = '';
+let playerSelection = "";
+let computerSelection = "";
 
 function isRock() {
-    playerSelection = 'rock';
-    computerSelection = getComputerChoice();
-    displayResult(playGame(playerSelection, computerSelection));
-};
+  playerSelection = "rock";
+  computerSelection = getComputerChoice();
+  displayResult(playGame(playerSelection, computerSelection));
+}
 
 function isPaper() {
-    playerSelection = 'paper';
-    computerSelection = getComputerChoice();
-    displayResult(playGame(playerSelection, computerSelection));
-};
+  playerSelection = "paper";
+  computerSelection = getComputerChoice();
+  displayResult(playGame(playerSelection, computerSelection));
+}
 
 function isScissors() {
-    playerSelection = 'scissors';
-    computerSelection = getComputerChoice();
-    displayResult(playGame(playerSelection, computerSelection));
-};
+  playerSelection = "scissors";
+  computerSelection = getComputerChoice();
+  displayResult(playGame(playerSelection, computerSelection));
+}
 
-rock.addEventListener('click', isRock);
+rock.addEventListener("click", isRock);
 
-paper.addEventListener('click', isPaper);
+paper.addEventListener("click", isPaper);
 
-scissors.addEventListener('click', isScissors);
-
+scissors.addEventListener("click", isScissors);
 
 /* This is the basic game logic */
-function playGame(playerSelection, computerSelection) { 
-    if (playerSelection === 'rock' && computerSelection === 'paper') {
-        return "You Lose! Paper beats rock.";
-        } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        return "You Win! Rock beats scissors.";
-        } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        return "You Win! Paper beats rock.";
-        } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        return "You Lose! Scissors beats paper.";
-        } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        return "You Lose! Rock beats scissors.";
-        } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        return "You Win! Scissors beats paper.";
-        } else {
-        return "It's a tie!";
-        }          
-    };
+function playGame(playerSelection, computerSelection) {
+  if (playerSelection === "rock" && computerSelection === "paper") {
+    return "You Lose! Paper beats rock.";
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "You Win! Rock beats scissors.";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "You Win! Paper beats rock.";
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "You Lose! Scissors beats paper.";
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return "You Lose! Rock beats scissors.";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "You Win! Scissors beats paper.";
+  } else {
+    return "It's a tie!";
+  }
+}
 
-    function displayResult(result) {
-        alert(`${result} Play Again?`);
-        console.log(playerSelection);
-        console.log(computerSelection);        
-    };
+function displayResult(result) {
+  alert(`${result} Play Again?`);
+  console.log(playerSelection);
+  console.log(computerSelection);
+}
 
-    
-    
-/* This is the function to play the game, using a loop to play bets of 5*/       
+/* This is the function to play the game, using a loop to play bets of 5*/
 /*function game() {
     let playerScore = 0;
     let computerScore = 0;
