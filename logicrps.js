bbc792eabb2612083ffe3016077c6031ca72e1f3
Hornsy96 <jakehorsnell96@gmail.com>
2023-06-05 21:57:12 +1000

    update html to display results in resultscreen added functions to check if score is 5 and reset game

--- logicrps.js
+++ logicrps.js
@@ -2,6 +2,14 @@ const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
 const display = document.querySelector("#resultscreen");
+const score = document.querySelector("#score");
+const game = document.querySelector("#game");
+
+let gameResult = "";
+let playerSelection = "";
+let computerSelection = "";
+let playerScore = 0;
+let computerScore = 0;
 
 /* Logic to randomise the computers choice */
 function getComputerChoice() {
@@ -15,25 +23,29 @@ function getComputerChoice() {
   }
 }
 
-let playerSelection = "";
-let computerSelection = "";
+function updateScore() {
+  score.textContent = (`${playerScore} - ${computerScore}`);
+}
 
 function isRock() {
   playerSelection = "rock";
   computerSelection = getComputerChoice();
   displayResult(playGame(playerSelection, computerSelection));
+  checkGameResult();
 }
 
 function isPaper() {
   playerSelection = "paper";
   computerSelection = getComputerChoice();
   displayResult(playGame(playerSelection, computerSelection));
+  checkGameResult();
 }
 
 function isScissors() {
   playerSelection = "scissors";
   computerSelection = getComputerChoice();
   displayResult(playGame(playerSelection, computerSelection));
+  checkGameResult();
 }
 
 rock.addEventListener("click", isRock);
@@ -45,54 +57,57 @@ scissors.addEventListener("click", isScissors);
 /* This is the basic game logic */
 function playGame(playerSelection, computerSelection) {
   if (playerSelection === "rock" && computerSelection === "paper") {
+    computerScore++;
+    game.textContent = ("You Lose! Paper beats rock.");
     return "You Lose! Paper beats rock.";
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    playerScore++;
+    game.textContent = ("You Win! Rock beats scissors.");
     return "You Win! Rock beats scissors.";
   } else if (playerSelection === "paper" && computerSelection === "rock") {
+    playerScore++;
+    game.textContent = ("You Win! Paper beats rock.");
     return "You Win! Paper beats rock.";
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    computerScore++;
+    game.textContent = ("You Lose! Scissors beats paper.");
     return "You Lose! Scissors beats paper.";
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    computerScore++;
+    game.textContent = ("You Lose! Rock beats scissors.");
     return "You Lose! Rock beats scissors.";
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    playerScore++;
+    game.textContent = ("You Win! Scissors beats paper.");
     return "You Win! Scissors beats paper.";
   } else {
+    game.textContent = ("It's a tie!");
     return "It's a tie!";
   }
 }
 
 function displayResult(result) {
-  alert(`${result} Play Again?`);
-  console.log(playerSelection);
-  console.log(computerSelection);
+  updateScore(); 
+  }
+
+function checkGameResult() {
+  if (playerScore === 5) {
+    gameResult = "You win!";
+    alert(gameResult);
+  } else if (computerScore === 5) {
+    gameResult = "You lose..";
+    alert(gameResult);
+  }  
+  
+  resetGame();
+  } 
+
+
+
+function resetGame() {
+  if (gameResult === "You win!" || gameResult === "You lose..") {
+    playerScore = 0;
+    computerScore = 0;
+  }
 }
 
-/* This is the function to play the game, using a loop to play bets of 5*/
-/*function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-
-    for (let i = 1; i <= 5; i++){
-        const result = playGame();
-        console.log(result);
-
-        if (result.includes('Win')) {
-            playerScore++;  
-        } else if (result.includes('Lose'))
-            computerScore++;
-        }
-
-    console.log(`Player score: ${playerScore}`);
-    console.log(`Computer score: ${computerScore}`);
-
-    if (playerScore > computerScore) {
-        return 'You win!'
-    } else if (computerScore > playerScore) {
-        return 'You lose!'
-    } else {
-        return 'Tie game..'
-    };
-};
-
-    console.log(game());
-*/
